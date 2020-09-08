c8c762f278abc0a052ab6237a98fe03794c51b2e
Maria <marylevchenko@gmail.com>
2020-09-08 23:45:47 +0300

    debug to true

--- litgid/settings.py
+++ litgid/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
