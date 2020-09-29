667b4e8eeb9187d4d0f20f570d7b636ef79ceb3b
Maria <marylevchenko@gmail.com>
2020-09-29 21:45:16 +0300

    edit graph

--- core/net.py
+++ core/net.py
@@ -1,34 +1,52 @@
 import plotly.graph_objs as go
 import networkx as nx
+import random
 
-G = nx.random_geometric_graph(200, 0.125)
+#G = nx.random_geometric_graph(200, 0.125)
+G = nx.Graph()
 
-edge_x = []
-edge_y = []
-for edge in G.edges():
-    x0, y0 = G.nodes[edge[0]]['pos']
-    x1, y1 = G.nodes[edge[1]]['pos']
-    edge_x.append(x0)
-    edge_x.append(x1)
-    edge_x.append(None)
-    edge_y.append(y0)
-    edge_y.append(y1)
-    edge_y.append(None)
+G.add_edge('First', 'Second', weight=1)
+G.add_edge('Second', 'Third', weight=1)
+G.add_edge('Third', 'Fourth', weight=1)
+G.add_edge('Fourth', 'First', weight=1)
+G.add_edge('One', 'Two', weight=2)
 
-edge_trace = go.Scatter(
+layt = nx.spring_layout(G, dim=2) # Generates the layout of the graph
+Xn = [layt[k][0] for k in list(layt.keys())]  # x-coordinates of nodes
+Yn = [layt[k][1] for k in list(layt.keys())]  # y-coordinates
+Xe = []
+Ye = []
+
+plot_weights = []
+for e in G.edges():
+    Xe += [layt[e[0]][0], layt[e[1]][0], None]
+    Ye += [layt[e[0]][1], layt[e[1]][1], None]
+    ax = (layt[e[0]][0]+layt[e[1]][0])/2
+    ay = (layt[e[0]][1]+layt[e[1]][1])/2
+    plot_weights.append((random.randrange(3), ax, ay))
+
+"""edge_trace = go.Scatter(
     x=edge_x, y=edge_y,
     line=dict(width=0.5, color='#888'),
     hoverinfo='none',
-    mode='lines')
+    mode='lines')"""
 
-node_x = []
+edge_trace = go.Scatter(
+    x=Xe,
+    y=Ye,
+    mode='lines',
+    line=dict(color='rgb(90, 90, 90)', width=1),
+    hoverinfo='none'
+    )
+
+"""node_x = []
 node_y = []
 for node in G.nodes():
     x, y = G.nodes[node]['pos']
     node_x.append(x)
-    node_y.append(y)
+    node_y.append(y)"""
 
-node_trace = go.Scatter(
+"""node_trace = go.Scatter(
     x=node_x, y=node_y,
     mode='markers',
     hoverinfo='text',
@@ -48,6 +66,23 @@ node_trace = go.Scatter(
             xanchor='left',
             titleside='right'
         ),
-        line_width=2))
+        line_width=2))"""
+
+node_trace = go.Scatter(
+    x=Xn,
+    y=Yn,
+    mode='markers+text',
+    name='Nodes',
+    marker=dict(symbol='circle',
+        size=8,
+        #color=group,
+        colorscale='Viridis',
+        line=dict(color='rgb(255,255,255)', width=1)
+        ),
+    text="test",
+    textposition='top center',
+    hoverinfo='none'
+    )
+
 
 data = [edge_trace, node_trace]
